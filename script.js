ea2ddedeb58271c351ad5938d1adb2fea20dfbb1
Michael Sewell <michael.sewell@jandjbagging.com>
2026-07-16 13:21:34 -0500

    Created the base of the app.

---
+++ script.js
@@ -0,0 +1,5 @@
+import * as utilities from "./JS/utilities.js";
+import * as contentView from "./JS/content view.js";
+import * as account from "./JS/account.js";
+
+contentView.updateContentField(document.querySelector(".tab.active").id);
